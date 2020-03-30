5c09616a49dd9d3f7e10d4a9a1c6b70323410ed0
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2020-03-30 12:19:09 +0300

    add react router and first route

---
+++ src/pages/HomePage.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const HomePage = () => (
+  <>
+    <h1>Hello, Welcome to my Blog</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus posuere,
+      dolor vel scelerisque aliquam, erat nisi tempus justo, in imperdiet nunc
+      tortor id velit. Nulla ultricies mauris at consequat tempor. Nulla ac
+      rhoncus nulla, non tempor mauris. Praesent vitae dictum nisl, in elementum
+      arcu. Praesent iaculis purus quis nisl efficitur laoreet. Interdum et
+      malesuada fames ac ante ipsum primis in faucibus. Praesent faucibus congue
+      orci a iaculis. Aliquam ut magna tristique, viverra velit vitae, lobortis
+      ligula.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras bibendum
+      lectus nisi, accumsan viverra tortor dignissim blandit. Aenean nec risus a
+      metus lacinia posuere eu eget nulla. Sed auctor lorem et dolor
+      pellentesque, quis finibus mauris vulputate. Donec posuere nulla eget arcu
+      iaculis eleifend nec cursus nunc. Proin porta, urna quis rutrum porta,
+      odio eros fermentum tellus, ut molestie tellus nisi sed est. Nulla
+      vestibulum mauris non nisl tincidunt, in bibendum mauris dictum. Phasellus
+      tempus metus et tortor cursus tristique. Cras sed ipsum ipsum. In tempus
+      nec urna sed facilisis.
+    </p>
+    <p>
+      Pellentesque eu nisi urna. Suspendisse potenti. Phasellus massa velit,
+      luctus id nisi id, fringilla ullamcorper odio. Phasellus at massa et
+      tortor ultricies pulvinar. Aenean tristique nec ante a aliquet. Praesent
+      suscipit molestie mauris sit amet aliquam. Morbi dapibus laoreet erat, non
+      dapibus sapien luctus in. Duis vitae ipsum commodo, convallis massa nec,
+      auctor quam. Aliquam ut mi eget ligula pharetra pulvinar. Duis at lectus
+      non nisi porttitor vulputate eu ac turpis.
+    </p>
+  </>
+);
+
+export default HomePage;
