008063d3b87c4831f051a6a0d6a10acb32db9cb9
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2020-03-31 11:08:01 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlesList from "./pages/ArticlesList";
+import ArticlePage from "./pages/ArticlePage";
+import NavBar from "./NavBar";
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Route path="/" exact>
-          <HomePage />
-        </Route>
+const App = () => (
+  <Router>
+    <div className="App">
+      <NavBar />
+      <div id="page-body">
+        <Route path="/" component={HomePage} exact />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/articles-list" component={ArticlesList} />
+        <Route path="/article/:name" component={ArticlePage} />
       </div>
-    </Router>
-  );
-}
+    </div>
+  </Router>
+);
 
 export default App;
