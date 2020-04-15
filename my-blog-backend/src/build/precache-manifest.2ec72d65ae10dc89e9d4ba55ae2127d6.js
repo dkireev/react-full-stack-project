e596bca7e565509da9819649197703f178d85b6d
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2020-04-15 13:02:51 +0300

    first build release

---
+++ my-blog-backend/src/build/precache-manifest.2ec72d65ae10dc89e9d4ba55ae2127d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b65f4f3505939a1b038c9811c7ac48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceac938f6e5e9996b70c",
+    "url": "/static/css/main.7b5f7dc8.chunk.css"
+  },
+  {
+    "revision": "4dc7ef4d7f602c5fe050",
+    "url": "/static/js/2.c3a0f8d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c3a0f8d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceac938f6e5e9996b70c",
+    "url": "/static/js/main.6429d45b.chunk.js"
+  },
+  {
+    "revision": "85cfd258e161521f816c",
+    "url": "/static/js/runtime-main.7c119f90.js"
+  }
+]);
